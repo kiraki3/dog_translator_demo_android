ed0f85ef7f9f7ab4151960356e8fd4d6e2e60739
doyou1 <tla149@daum.net>
2024-08-17 16:39:00 +0900

    - DogProfileRegisterActivity cleanup - `strings.xml`, `dimens.xml` 활용

--- app/src/main/java/com/example/dogtranslator/DogProfileRegisterActivity.java
+++ app/src/main/java/com/example/dogtranslator/DogProfileRegisterActivity.java
@@ -3,8 +3,6 @@ package com.example.dogtranslator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,34 +10,38 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.dogtranslator.databinding.ActivityDogProfileRegisterBinding;
+
 public class DogProfileRegisterActivity extends AppCompatActivity {
 
-    private ImageView iv_logo;
-    private Button btn_register;
+    private ActivityDogProfileRegisterBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_dog_profile_register);
+        setDataBinding();
+        setClickEvent();
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
 
-        // 버튼 필드 초기화
-        btn_register = findViewById(R.id.btn_register);
+    private void setDataBinding() {
+        binding = ActivityDogProfileRegisterBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+    }
 
-        // 프로필 입력 버튼 클릭 리스너
-        btn_register.setOnClickListener(new View.OnClickListener() {
+    private void setClickEvent() {
+        binding.btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(DogProfileRegisterActivity.this, DogProfileSettingActivity.class);
                 startActivity(intent); // move Activity
             }
         });
-
-
     }
 }
--- app/src/main/java/com/example/dogtranslator/MainActivity.java
+++ app/src/main/java/com/example/dogtranslator/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.dogtranslator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,20 +13,20 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.dogtranslator.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-//    private Button btn_start;
+
     private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+        setDataBinding();
+        setClickEvent();
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        setDataBinding();
-        setClickEvent();
     }
 
     private void setDataBinding() {
@@ -45,6 +44,5 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
 
 }
