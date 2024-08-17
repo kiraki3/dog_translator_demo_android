500f9228233878f9057bd7554a8b4674697888f4
doyou1 <tla149@daum.net>
2024-08-17 19:26:30 +0900

    - when EditText focus out, hide keyboard

--- app/src/main/java/com/example/dogtranslator/DogProfileSettingActivity.java
+++ app/src/main/java/com/example/dogtranslator/DogProfileSettingActivity.java
@@ -1,10 +1,15 @@
 package com.example.dogtranslator;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Rect;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.graphics.Bitmap;
 import android.widget.Toast;
@@ -100,5 +105,22 @@ public class DogProfileSettingActivity extends AppCompatActivity {
                 binding.ivPuppyImage.setImageResource(R.drawable.puppy_logo); // 이미지 로딩 오류 시 기본 이미지로 설정
             }
         }
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            View v = getCurrentFocus();
+            if ( v instanceof EditText) {
+                Rect outRect = new Rect();
+                v.getGlobalVisibleRect(outRect);
+                if (!outRect.contains((int)event.getRawX(), (int)event.getRawY())) {
+                    v.clearFocus();
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+            }
+        }
+        return super.dispatchTouchEvent( event );
     }
 }
