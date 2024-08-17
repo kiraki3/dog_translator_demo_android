d0b191f8392b599559c380ba63dac901cbea508b
doyou1 <tla149@daum.net>
2024-08-17 16:27:46 +0900

    - MainActivity cleanup - `strings.xml`, `dimens.xml` 활용

--- app/src/main/java/com/example/dogtranslator/MainActivity.java
+++ app/src/main/java/com/example/dogtranslator/MainActivity.java
@@ -21,18 +21,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        setDataBinding();
+        setClickEvent();
+    }
 
+    private void setDataBinding() {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+    }
 
-        binding.btnStart.setOnClickListener(new View.OnClickListener() {
+    private void setClickEvent() {
+        binding.btnMainStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, DogProfileRegisterActivity.class);
@@ -40,4 +45,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+
 }
